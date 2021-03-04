6ef0903e17e6e4ec399274c24c6fa4d534448dd5
dotoryeee <bayer@kakao.com>
2021-03-04 12:02:25 +0000

    vpc subnet

--- 210304/vpc.tf
+++ 210304/vpc.tf
@@ -1,6 +1,26 @@
 resource "aws_vpc" "terra-test" {
 	cidr_block = "10.0.0.0/16"
+
 	tags = {
 		Name = "Terraform TEST1"
 	}
+}
+
+resource"aws_subnet" "public01" {
+	vpc_id = aws_vpc.terra-test.id
+	cidr_block = "10.0.0.0/24"
+	availability_zone = "ap-northeast-2a"
+
+	tags = {
+		Name = "terra-public-01-subnet"
+	}
+}
+
+resource "aws_subnet" "private01" {
+	vpc_id = aws_vpc.terra-test.id
+        cidr_block = "10.0.1.0/24"
+
+        tags = {
+                Name = "terra-private-01-subnet"
+        }
 }
