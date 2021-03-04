a6d280042cfc4204322ac3a30095de00f230134b
dotoryeee <bayer@kakao.com>
2021-03-04 14:55:43 +0000

    finish vpc

--- 210304/vpc.tf
+++ 210304/vpc.tf
@@ -51,3 +51,40 @@ resource "aws_nat_gateway" "nat" {
 	}
 }
 
+resource "aws_route_table" "public"{
+	vpc_id = aws_vpc.terra-test.id
+	
+	route {
+	cidr_block = "0.0.0.0/0"
+	gateway_id = aws_internet_gateway.igw.id
+	}
+
+	tags = {
+		Name = "terra_public_rt"
+	}
+}
+
+resource "aws_route_table_association" "public_association"{
+	subnet_id = aws_subnet.public01.id
+	route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table" "private"{
+	vpc_id = aws_vpc.terra-test.id
+
+	tags = {
+		Name = "terra_private_rt"
+	}
+}
+
+resource "aws_route_table_association" "private_association" {
+	subnet_id = aws_subnet.private01.id
+	route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route" "private_nat" {
+	route_table_id = aws_route_table.private.id
+	destination_cidr_block = "0.0.0.0/0"
+	nat_gateway_id = aws_nat_gateway.nat.id
+}
